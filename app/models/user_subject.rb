91cb86e31a77cf0dc45a58b2b71d6d948e2937a0
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-04-29 12:17:42 +0900

    fix: UserSubjectに不足していたバリデーションを追加x

--- app/models/user_subject.rb
+++ app/models/user_subject.rb
@@ -8,6 +8,10 @@ class UserSubject < ApplicationRecord
 
     validates :subject_name, presence: true
     validates :credits, numericality: { in: 0..4 }
+    validates :user, presence: true
+    validates :schedule, presence: true
+    validates :semester, presence: true
+    validates :required_subject, presence: true
 
     enum :grade, [ :S, :A, :B, :C, :D, :F ]
     enum :day, [ :Mon, :Tue, :Wed, :Thu, :Fri, :Sat, :Sun ]
