76398e9a0d86f3a433d9fe9964156f9038e16168
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-05-04 16:27:07 +0900

    fix: TailwindのInput系がバグってたのを修正する

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         './app/views/**/*.html.erb',
         './app/helpers/**/*.rb',
         './app/assets/stylesheets/**/*.css',
-        './app/javascript/**/*.js',
-        './app/javascript/**/*.jsx',
-        './app/javascript/**/*.tsx'
-    ]
+        './app/javascript/**/*.{js,jsx,ts,tsx}'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/container-queries'),
+    ],
 }
