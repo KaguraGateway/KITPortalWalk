0d08a1977698c9f25a65f76160b11858662ff545
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-05-04 14:43:07 +0900

    fix: Turboを使えるように修正

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,11 +1,13 @@
 // Entry point for the build script in your package.json
+import "@hotwired/turbo-rails";
+
 import React, { isValidElement, cloneElement } from "react";
 import { createRoot } from "react-dom/client";
 import { GraduationRequirementForm } from "./react/components/admin/GraduationRequirementForm"
 const components: { [key: string]: React.FC<any> } = {
     "GraduationRequirementForm": GraduationRequirementForm
 };
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("turbo:load", () => {
     const reactRootElem = document.getElementById("react-root");
     if (!reactRootElem)
         return;
