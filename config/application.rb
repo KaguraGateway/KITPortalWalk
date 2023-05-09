cf4d82073721c9c08c9c8d5cc0c42c9aeb4555b0
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-05-09 09:06:00 +0900

    fix: TimeZoneをTokyoに変更

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Myapp
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb, yml}')]
