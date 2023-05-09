a619a672ebea0e0b9f5c67cf7e27cd7008cb076e
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-05-09 11:06:40 +0900

    feat: 時間割を実装

--- app/controllers/admin/class_schedules_controller.rb
+++ app/controllers/admin/class_schedules_controller.rb
@@ -1,7 +1,32 @@
 class Admin::ClassSchedulesController < Admin::ApplicationController
   def index
+    @class_schedules = ClassSchedule.all
   end
 
   def new
+    @class_schedule = ClassSchedule.new
+  end
+
+  def create
+    @class_schedule = ClassSchedule.new(user_params)
+    if @class_schedule.save
+        redirect_to admin_class_schedules_path
+    else
+        render :new
+    end
+  end
+
+  def destroy
+    class_schedule = ClassSchedule.find(params[:id])
+    if class_schedule.destroy
+      redirect_to admin_class_schedules_path, notice: "Class Schedule was deleted successfully."
+    else
+      redirect_to admin_class_schedules_path, alert: "Class Schedule could not be deleted."
+    end
+  end
+
+  private
+  def user_params
+    params.require(:class_schedule).permit(:name, :start_time, :end_time)
   end
 end
