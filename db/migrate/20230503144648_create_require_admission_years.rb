4428c920e0f1ac4ee45d191baac2109fc85964da
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-08-14 22:58:37 +0900

    fix: migrateできない問題を修正

--- db/migrate/20230503144648_create_require_admission_years.rb
+++ db/migrate/20230503144648_create_require_admission_years.rb
@@ -1,11 +1,11 @@
-class CreateRequiredAdmissionYears < ActiveRecord::Migration[7.0]
+class CreateRequireAdmissionYears < ActiveRecord::Migration[7.0]
   def change
-    create_table :required_admission_years do |t|
+    create_table :require_admission_years do |t|
       t.integer :year
       t.string :external_text
 
       t.timestamps
     end
-    add_index :required_admission_years, :year, unique: true
+    add_index :require_admission_years, :year, unique: true
   end
 end
